c04e7fb967cbe6363049cf3d4b2a0542d9496d51
jlnko <jlnko@hotmail.fr>
2024-08-16 14:05:50 +0200

    création du projet, implémentation du theme et du dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/src/theme/ThemeProvider";
+import clsx from "clsx";
+import { Header } from "@/src/feature/layout/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={clsx(inter.className, 'bg-background h-full')}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="flex flex-col h-full">
+          <Header />
+          <div className="flex-1 max-w-lg m-auto py-12 w-full">
+            {children}
+          </div>
+        </div>
+      </ThemeProvider>
+        </body>
     </html>
   );
 }
---
+++ src/feature/layout/Header.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggle } from '@/src/theme/ThemeToggle';
+
+export const Header = async () => {
+  return (
+    <header className='border-b border-b-accent'>
+        <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
+            <h2 className='text-2xl font-bold mr-auto'>Githread</h2>
+            <ThemeToggle />
+        </div>
+    </header>
+  )
+}
+
