29e3a6bb859232d9a49dcd0b5146635ab6590f4a
jlnko <jlnko@hotmail.fr>
2024-08-16 14:25:01 +0200

    création du footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/src/theme/ThemeProvider";
 import clsx from "clsx";
-import { Header } from "@/src/feature/layout/Header";
+import { Header } from "@/src/features/layout/Header";
+import { Footer } from "@/src/features/layout/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +27,7 @@ export default function RootLayout({
           <div className="flex-1 max-w-lg m-auto py-12 w-full">
             {children}
           </div>
+          <Footer />
         </div>
       </ThemeProvider>
         </body>
--- src/theme/ThemeToggle.tsx
+++ src/theme/ThemeToggle.tsx
@@ -14,7 +14,8 @@ export const ThemeToggle = () => {
         setTheme(theme === "light" ? "dark" : "light")
     }}>
         <SunMedium size={20} className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-        <Moon size={20} className="rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+        <Moon size={20} className="absolute rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+        <span className="sr-only">Toggle Theme</span>
     </Button>
   )
 }
